7755b75e720deff268e745281d88bebeb2872c86
Jamie Bullock <jamie@jamiebullock.com>
2014-08-24 22:00:12 +0100

    Initial Commit

---
+++ k-means/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  k-means
+//
+//  Created by Jamie Bullock on 24/08/2014.
+//  Copyright (c) 2014 Jamie Bullock. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
