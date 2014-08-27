af37feed18a95f15ca5ec798ab29e4351c8b335c
Jamie Bullock <jamie@jamiebullock.com>
2014-08-27 11:22:41 +0100

    Add setup code for initial centroids

--- k-means/main.c
+++ k-means/main.c
@@ -10,6 +10,24 @@
 
 #include <stdio.h>
 
+enum cluster_id {
+    Adam,
+    Bob,
+    Charley,
+    David,
+    Edward,
+    km_num_cluster_ids_
+};
+
+void set_initial_cluster_centroids(km_pointlist pointlist)
+{
+    km_pointlist_update(pointlist, 0, Adam, -0.357, -0.253);
+    km_pointlist_update(pointlist, 1, Bob, -0.055, 4.392);
+    km_pointlist_update(pointlist, 2, Charley, 2.674, -0.001);
+    km_pointlist_update(pointlist, 3, David, 1.044, -1.251);
+    km_pointlist_update(pointlist, 3, Edward, -1.495, -0.090);
+}
+
 int main(int argc, const char * argv[])
 {
 
@@ -19,13 +37,14 @@ int main(int argc, const char * argv[])
     RETURN_ON_ERROR(km_textfile_read(textfile, "input-2.csv"));
     
     km_pointlist pointlist = km_pointlist_new(km_textfile_num_lines(textfile));
+    km_pointlist centroids = km_pointlist_new(km_num_cluster_ids_);
     
     RETURN_ON_ERROR(km_pointlist_fill(pointlist, textfile));
-
+    
+    set_initial_cluster_centroids(centroids);
         
     km_pointlist_delete(pointlist);
     km_textfile_delete(textfile);
-    
-    
+        
     return 0;
 }
