e0bb0a109c5925515d3d3597263f46f070b859ff
Jamie Bullock <jamie@jamiebullock.com>
2014-08-27 10:53:31 +0100

    Include number of points in pointlist struct

--- k-means/km.c
+++ k-means/km.c
@@ -18,22 +18,17 @@ struct km_point_
     float y;
 };
 
+struct km_pointlist_
+{
+    struct km_point_ *points;
+    uint64_t num_points;
+};
+
 // 1. Read the data from file into N km_point structs
 // 2. Compute the distance of each point from each of M cluster centres
 // 3. Re-compute cluster centres
 // 4. Repeat 2–3 until the total error is minimised
 // 5. Write output points to file
-
-km_point km_point_new(void)
-{
-    return malloc(sizeof(struct km_point_));
-}
-
-
-void km_point_delete(km_point point)
-{
-    free(point);
-}
 
 km_error km_point_init(km_point point, uint32_t id, float x, float y)
 {
@@ -47,7 +42,16 @@ km_error km_point_init(km_point point, uint32_t id, float x, float y)
 
 km_pointlist km_pointlist_new(uint64_t num_points)
 {
-    return malloc(num_points * sizeof(struct km_point_ *));
+    km_pointlist pointlist = malloc(sizeof(struct km_pointlist_));
+    pointlist->num_points = num_points;
+    pointlist->points = malloc(num_points * sizeof(struct km_point_));
+    return pointlist;
+}
+
+void km_pointlist_delete(km_pointlist pointlist)
+{
+    free(pointlist->points);
+    free(pointlist);
 }
 
 km_error km_pointlist_fill(km_pointlist pointlist, km_textfile textfile)
@@ -63,9 +67,7 @@ km_error km_pointlist_fill(km_pointlist pointlist, km_textfile textfile)
     {
         sscanf(line, "%g,%g", &x, &y);
         free(line);
-        km_point point = km_point_new();
-        km_point_init(point, 0, x, y);
-        pointlist[count++] = point;
+        km_point_init(&pointlist->points[count++], 0, x, y);
         line = NULL;
     }
     
--- k-means/km.h
+++ k-means/km.h
@@ -1,6 +1,6 @@
 //
-//  k-means.h
-//  k-means
+//  km.h
+//  km
 //
 //  Created by Jamie Bullock on 24/08/2014.
 //  Copyright (c) 2014 Jamie Bullock. All rights reserved.
@@ -10,14 +10,17 @@
 #define KM_H
 
 #include "km_textfile.h"
+#include "km_error.h"
 
 #include <stdint.h>
 
-typedef struct km_point_ *km_point;
-typedef struct km_point_ **km_pointlist;
 #define RETURN_ON_ERROR(func) { km_error error__ = (func); if (error__ != km_NoError) return error__; }
 
+typedef struct km_point_ * km_point;
+typedef struct km_pointlist_ * km_pointlist;
+
 km_pointlist km_pointlist_new(uint64_t num_points);
+void km_pointlist_delete(km_pointlist pointlist);
 km_error km_pointlist_fill(km_pointlist pointlist, km_textfile textfile);
 uint64_t km_pointlist_num_points(km_pointlist pointlist);
 
