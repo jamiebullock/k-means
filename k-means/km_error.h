d693eec8c4968587d937df9fcef40aa9252db499
Jamie Bullock <jamie@jamiebullock.com>
2014-08-27 10:17:30 +0100

    Label errors

--- k-means/km_error.h
+++ k-means/km_error.h
@@ -11,16 +11,17 @@
 
 typedef enum km_error_
 {
-    km_NoError,
-    km_FileReadError,
-    km_FileCloseError,
-    km_FileSeekError,
-    km_FileTellError,
-    km_FileEndError,
-    km_MemoryAllocationError,
-    km_UnspecifiedError
+    km_NoError,                 // 0
+    km_FileReadError,           // 1
+    km_FileCloseError,          // 2
+    km_FileSeekError,           // 3
+    km_FileTellError,           // 4
+    km_FileEndError,            // 5
+    km_MemoryAllocationError,   // 6
+    km_UnspecifiedError         // 7
 }
 km_error;
 
 const char *km_error_to_string(km_error error);
+
 #endif // #ifndef KM_ERROR_H
